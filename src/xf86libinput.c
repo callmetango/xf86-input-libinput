0bcb60e744991c8d1127aa657de8c6b159649027
Martin Rys <martin@rys.pw>
2024-09-03 14:11:52 +0200

    Add support for LIBINPUT_LED_COMPOSE/LIBINPUT_LED_KANA
    Added in libinput 1.26
    
    Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-input-libinput/-/merge_requests/58>

--- src/xf86libinput.c
+++ src/xf86libinput.c
@@ -1170,14 +1170,19 @@ xf86libinput_init_pointer_absolute(InputInfoPtr pInfo)
 static void
 xf86libinput_kbd_ctrl(DeviceIntPtr device, KeybdCtrl *ctrl)
 {
-#define CAPSFLAG	1
-#define NUMFLAG		2
-#define SCROLLFLAG	4
-
+#define CAPSFLAG    1
+#define NUMFLAG     2
+#define SCROLLFLAG  4
+#define COMPOSEFLAG 8
+#define KANAFLAG    16
 	static struct { int xbit, code; } bits[] = {
 		{ CAPSFLAG,	LIBINPUT_LED_CAPS_LOCK },
 		{ NUMFLAG,	LIBINPUT_LED_NUM_LOCK },
 		{ SCROLLFLAG,	LIBINPUT_LED_SCROLL_LOCK },
+#ifdef HAVE_LIBINPUT_COMPOSE_AND_KANA
+		{ COMPOSEFLAG,	LIBINPUT_LED_COMPOSE },
+		{ KANAFLAG,	LIBINPUT_LED_KANA },
+#endif
 		{ 0, 0 },
 	};
 	int i = 0;
